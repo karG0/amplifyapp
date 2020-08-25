8180cf1aa3df93086ff4017fcfbde35310ba7a94
karthik5522 <karthik157v@gmail.com>
2020-08-25 13:16:33 +0530

    aws completed

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "us-east-2",
     "aws_user_pools_id": "us-east-2_qbAezA8oL",
     "aws_user_pools_web_client_id": "3v2lbct0h7is2lcn6k2g3kjsg2",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://q4dds6zcujb2tdldusql3hlmfy.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-qwipumzlf5bxrpqhyunbpefnye",
+    "aws_user_files_s3_bucket": "amplifyapp79a04349eb7f41959b6de63da4c9c21d232627-dev",
+    "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
 
